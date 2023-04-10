a22953b5645ffaa74aa0ffbf872cdb5dfc78f0fa
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-04-10 10:34:31 +0200

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -2,4 +2,5 @@ import streamlit as st
 st.text("hola mundo!")
 st.write("hola mundo")
 st.caption("esto es un caption")
-st.markdown("#jajajaja")
+st.markdown("😜:alien")
+st.radio("elige:",["a","b","c"],index=1)
