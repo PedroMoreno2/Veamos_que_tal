d8a5bbcf0a3f920a8e230c721776d5c0578bf3f2
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-04-10 10:38:13 +0200

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -2,5 +2,10 @@ import streamlit as st
 st.text("hola mundo!")
 st.write("hola mundo")
 st.caption("esto es un caption")
-st.markdown("😜:alien")
-st.radio("elige:",["a","b","c"],index=1)
+st.markdown("😜:alien:")
+ele=st.radio("elige:",["a","b","c"],index=1)
+if ele=="a":
+  st.text("elegiste aaa")
+elif ele=="b":
+  st.markdown("elegiste :red[bbbb]")
+  
