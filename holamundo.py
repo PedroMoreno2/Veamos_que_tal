8f86f45194964f4ad940d91cbd8c0ba5bc55f7bd
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-04-10 10:43:20 +0200

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -3,9 +3,11 @@ st.text("hola mundo!")
 st.write("hola mundo")
 st.caption("esto es un caption")
 st.markdown("😜:alien:")
-ele=st.radio("elige:",["a","b","c"],index=1)
-if ele=="a":
+
+listaopciones=["a","b","c"]
+ele=st.radio("elige:",listaopciones,index=1)
+if ele==listaopciones[0]:
   st.text("elegiste aaa")
-elif ele=="b":
+elif ele==listaopciones[1]:
   st.markdown("elegiste :red[bbbb]")
   
